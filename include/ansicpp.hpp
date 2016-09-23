3301bf0a79540092e5fe5c5360702052dd111a1f
Claus Witt <claus@wittnezz.dk>
2016-09-23 15:19:43 +0200

    initial commit

---
+++ include/ansicpp.hpp
@@ -0,0 +1,9 @@
+#ifndef ANSICPP_ANSICPP
+#define ANSICPP_ANSICPP 1
+class Ansi {
+  public:
+    Ansi();
+
+
+};
+#endif /* ifndef ANSICPP_ANSICPP */
---
+++ src/ansicpp.cpp
@@ -0,0 +1,5 @@
+#include <ansicpp.hpp>
+
+Ansi::Ansi() {
+
+}
