e5f210d7a51e8d7eda31c7ce34b55785e6e51440
Claus Witt <claus@wittnezz.dk>
2016-11-07 09:54:23 +0100

    makes the library header only

--- include/ansicpp.hpp
+++ include/ansicpp.hpp
@@ -36,26 +36,53 @@ namespace ansicpp {
   class Ansi {
     private:
       std::string ESC = "\033" ;
-      std::string escape(std::string value);
+      std::string escape(std::string value) {
+        return ESC + '[' + value;
+      }
 
     public:
-      Ansi();
-      std::string clear();
-      std::string show_cursor();
-      std::string hide_cursor();
-      std::string clear_line();
-      std::string cursor_up(int count=1);
-      std::string cursor_down(int count=1);
-      std::string cursor_forward(int count=1);
-      std::string cursor_back(int count=1);
+      std::string clear() {
+        return escape("2J");
+      }
+      std::string show_cursor() {
+        return escape("?25h");
+      }
+      std::string hide_cursor() {
+        return escape("?25l");
+      }
+      std::string clear_line() {
+        return escape("2K");
+      }
+      std::string cursor_up(int count=1) {
+        return escape(std::to_string(count)+"A");
+      }
+      std::string cursor_down(int count=1) {
+        return escape(std::to_string(count)+"B");
+      }
+      std::string cursor_forward(int count=1) {
+        return escape(std::to_string(count)+"C");
+      }
+      std::string cursor_back(int count=1) {
+        return escape(std::to_string(count)+"D");
+      }
 
-      std::string cursor_next_line(int count=1);
-      std::string cursor_prev_line(int count=1);
+      std::string cursor_next_line(int count=1) {
+        return escape(std::to_string(count)+"E");
+      }
+      std::string cursor_prev_line(int count=1) {
+        return escape(std::to_string(count)+"F");
+      }
 
-      std::string cursor_abs_col(int count=1);
-      std::string cursor_abs_pos(int row=1, int col=1);
+      std::string cursor_abs_col(int count=1) {
+        return escape(std::to_string(count)+"G");
+      }
+      std::string cursor_abs_pos(int row=1, int col=1) {
+        return escape(std::to_string(row)+";" +std::to_string(col)+"H");
+      }
 
-      std::string set_color(int fg=FG::Colors::default_color, int bg=BG::Colors::default_color);
+      std::string set_color(int fg=FG::Colors::default_color, int bg=BG::Colors::default_color) {
+        return escape(std::to_string(fg) + ";" + std::to_string(bg) + "m");
+      }
 
 
 
