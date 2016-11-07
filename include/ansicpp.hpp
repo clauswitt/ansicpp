5703aa9e33b2c94c4555f750e9f87dbd7f33e24a
Claus Witt <claus@wittnezz.dk>
2016-11-07 10:11:12 +0100

    make everything static

--- include/ansicpp.hpp
+++ include/ansicpp.hpp
@@ -34,56 +34,64 @@ namespace ansicpp {
   }
   class Ansi {
     private:
-      std::string ESC = "\033" ;
-      std::string escape(std::string value) {
-        return ESC + '[' + value;
+      static std::string escape(std::string value) {
+        std::string ESC = "\033";
+        return ESC + "[" + value;
       }
 
     public:
-      std::string clear() {
+
+      static std::string clear() {
         return escape("2J");
       }
-      std::string show_cursor() {
+      static std::string show_cursor() {
+
         return escape("?25h");
       }
-      std::string hide_cursor() {
+
+      static std::string hide_cursor() {
         return escape("?25l");
       }
-      std::string clear_line() {
+
+      static std::string clear_line() {
         return escape("2K");
       }
-      std::string cursor_up(int count=1) {
+
+      static std::string cursor_up(int count=1) {
         return escape(std::to_string(count)+"A");
       }
-      std::string cursor_down(int count=1) {
+
+      static std::string cursor_down(int count=1) {
         return escape(std::to_string(count)+"B");
       }
-      std::string cursor_forward(int count=1) {
+
+      static std::string cursor_forward(int count=1) {
         return escape(std::to_string(count)+"C");
       }
-      std::string cursor_back(int count=1) {
+
+      static std::string cursor_back(int count=1) {
         return escape(std::to_string(count)+"D");
       }
 
-      std::string cursor_next_line(int count=1) {
+      static std::string cursor_next_line(int count=1) {
         return escape(std::to_string(count)+"E");
       }
-      std::string cursor_prev_line(int count=1) {
+
+      static std::string cursor_prev_line(int count=1) {
         return escape(std::to_string(count)+"F");
       }
 
-      std::string cursor_abs_col(int count=1) {
+      static std::string cursor_abs_col(int count=1) {
         return escape(std::to_string(count)+"G");
       }
-      std::string cursor_abs_pos(int row=1, int col=1) {
+
+      static std::string cursor_abs_pos(int row=1, int col=1) {
         return escape(std::to_string(row)+";" +std::to_string(col)+"H");
       }
 
-      std::string set_color(int fg=FG::Colors::default_color, int bg=BG::Colors::default_color) {
+      static std::string set_color(int fg=FG::Colors::default_color, int bg=BG::Colors::default_color) {
         return escape(std::to_string(fg) + ";" + std::to_string(bg) + "m");
       }
-
-
 
   };
 }
